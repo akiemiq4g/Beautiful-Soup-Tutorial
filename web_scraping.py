06ce6c70a63e10fc1ea7ee1816407b10b7be48ab
Akiem Watley <akiemiq4g@icloud.com>
2023-08-10 07:41:45 -0400

    still working on web_scraping5.py

--- web_scraping.py
+++ web_scraping.py
@@ -10,12 +10,13 @@ tags = doc.find_all("p")[0]
 print(tags.find_all("b"))
 
 # HTML From Website
-url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
+url = """https://www.bhphotovideo.com/c/search?Ntt=Apple%20Macbook%20Pro%20M1%20Pro%2016&N=0&InitialSearch=
+yes&ap=Y&gclid=Cj0KCQjwrMKmBhCJARIsAHuEAPSvPQvpNZYXwr7OxyYPWth9ncxicHMFLPzoSdFFPmueDBXrhZq_rGEaAq3eEALw_wcB"""
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
 prices = doc.find_all(text="$")
-parent = prices[0].parent
+parent = prices[0].parent()
 strong = parent.find("strong")
 print(strong.string)
